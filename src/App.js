f4fa0556bbc1915a78493a02f1507e07490a8267
PeterBranadal <42754479+PeterBranadal@users.noreply.github.com>
2020-05-15 10:34:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SalesForm from "./components/salesForm";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      {" "}
+      <SalesForm></SalesForm>
+    </div>
+  );
+}
+
+export default App;
