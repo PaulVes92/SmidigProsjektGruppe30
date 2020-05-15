aa1dd5d392ee08ed5c1df25daa90a092d3e4a17e
Paul Anders <paulanders92@gmail.com>
2020-05-15 10:14:03 +0200

    Fixed salesForm missing css

--- src/components/salesForm.jsx
+++ src/components/salesForm.jsx
@@ -1,38 +1,100 @@
-import React, { Component } from "react";
-import DatePicker from "react-date-picker";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-class SalesForm extends Component {
-  state = {
-    fromDate: new Date(),
-    toDate: new Date(),
-  };
+const SalesForm = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
-  onChange = (fromDate, toDate) => this.setState({ fromDate, toDate });
-
-  render() {
-    return (
-      <div>
-        <form>
-          <label>Fornavn</label>
-          <input type="text" placeholder="" />
-          <label>Etternavn</label>
-          <input type="text" placeholder="" />
-          <label>Email</label>
-          <input type="text" placeholder="" />
-          <label>Telefonnummer</label>
-          <input type="text" placeholder="" />
-          <DatePicker
-            onChange={this.onChange}
-            value={this.state.fromDate}
-          ></DatePicker>
-          <DatePicker
-            onChange={this.onChange}
-            value={this.state.toDate}
-          ></DatePicker>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <form className="form-group mr-2">
+        <label>Fornavn</label>
+        <input className="form-control" type="text" placeholder="" />
+        <label>Etternavn</label>
+        <input className="form-control" type="text" placeholder="" />
+        <label>Email</label>
+        <input className="form-control" type="text" placeholder="" />
+        <label>Telefonnummer</label>
+        <input className="form-control" type="text" placeholder="" />
+        <label>Fra dato</label>
+        <DatePicker
+          className="form-control"
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+        ></DatePicker>
+        <label>Til dato</label>
+        <DatePicker
+          className="form-control"
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+        ></DatePicker>
+        <br></br>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default SalesForm;
+
+// class SalesForm extends Component {
+//   state = {
+//     startDate: new Date(),
+//     endDate: new Date(),
+//     setStartDate: new Date(),
+//     setEndDate: new Date(),
+//   };
+
+//   handleChange = (date) => {
+//     this.setState({
+//       startDate: date,
+//       endDate: date,
+//     });
+//   };
+
+//   render() {
+//     const [startDate, setStartDate] = useState(new Date("2014/02/08"));
+//     const [endDate, setEndDate] = useState(new Date("2014/02/10"));
+
+//     return (
+//       <div>
+//         <form>
+//           <label>Fornavn</label>
+//           <input type="text" placeholder="" />
+//           <label>Etternavn</label>
+//           <input type="text" placeholder="" />
+//           <label>Email</label>
+//           <input type="text" placeholder="" />
+//           <label>Telefonnummer</label>
+//           <input type="text" placeholder="" />
+//           <label>Fra dato</label>
+//           <DatePicker
+//             selected={startDate}
+//             onChange={(date) => setStartDate(date)}
+//             selectsStart
+//             startDate={startDate}
+//             endDate={endDate}
+//           ></DatePicker>
+//           <DatePicker
+//             selected={endDate}
+//             onChange={(date) => setEndDate(date)}
+//             selectsEnd
+//             startDate={startDate}
+//             endDate={endDate}
+//             minDate={startDate}
+//           ></DatePicker>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// export default SalesForm;
