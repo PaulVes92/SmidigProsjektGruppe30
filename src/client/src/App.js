2e60a4934aad9552ea2d2b0d0126709a03491be0
Paul Anders <paulanders92@gmail.com>
2020-05-29 13:38:48 +0200

    fixing merge problems

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -4,31 +4,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Products from "./components/products";
 import Customers from "./components/customers";
-<<<<<<< HEAD
-import SalesForm from "./components/salesForm";
-import { Navigation } from "./components/navigation";
-=======
 import Navigation from "./components/navigation";
 import SalesForm from "./components/salesForm";
->>>>>>> c3636c52158de0bb45eef81b8e1caa4fff4864c4
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-      <Navigation />
-      <div id="salesForm" className="col-sm-12">
-        <SalesForm />
-      </div>
-
-      <div id="routes" className="col-sm-6">
-        <Switch>
-          <Route path="/customers" component={Customers} exact />
-          <Route path="/products" component={Products} />
-        </Switch>
-=======
       <div className="container-fluid p-0 ">
         <Navigation />
         <div className="row">
@@ -42,7 +25,6 @@ function App() {
             </Switch>
           </div>
         </div>
->>>>>>> c3636c52158de0bb45eef81b8e1caa4fff4864c4
       </div>
     </BrowserRouter>
   );
