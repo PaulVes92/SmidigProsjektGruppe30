c969437307bc1d9407c1849fe77f4629207c0d1a
Astrid <astrid.indrelid@gmail.com>
2020-05-29 13:25:07 +0200

    add reciepts.jsx and add route inn app.js

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Products from "./components/products";
 import Customers from "./components/customers";
 import Navigation from "./components/navigation";
+import Reciepts from "./components/reciepts";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
@@ -17,6 +18,7 @@ function App() {
         <Switch>
           <Route path="/customers" component={Customers} />
           <Route path="/products" component={Products} />
+          <Route path="/reciepts" component={Reciepts} />
         </Switch>
       </div>
     </BrowserRouter>
