b58d821c3d4774001bac5bb1386c4b17f0fcf8b7
kevin <kebra98@gmail.com>
2020-06-04 16:38:32 +0200

    Added login system, reverted customer table

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -8,9 +8,12 @@ import Navigation from "./components/navigation";
 import SalesForm from "./components/salesForm";
 import Reciepts from "./components/reciepts";
 import Rented from "./components/rented";
+import Login from "./components/login";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { renderIntoDocument } from "react-dom/test-utils";
+
+// submit --> start function App()
 
 function App() {
   return (
@@ -23,6 +26,7 @@ function App() {
           </div>
           <div className="col-md-10 pt-5 mt-5">
             <Switch>
+              <Route exact strict path="/" component={Login} />
               <Route path="/customers" component={Customers} />
               <Route path="/products" component={Products} />
               <Route path="/reciepts" component={Reciepts} />
---
+++ src/server/routes/login.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const Login = require("../db/login");
+const router = express.Router();
+
+module.exports = router;
