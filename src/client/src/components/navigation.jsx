e71967417e39a8fb27da957a8621700f46b86293
Astrid <astrid.indrelid@gmail.com>
2020-06-05 13:19:04 +0200

    change tabname to admin

--- src/client/src/components/navigation.jsx
+++ src/client/src/components/navigation.jsx
@@ -89,7 +89,7 @@ class Navigation extends Component {
             borderTopRightRadius: "45%",
           }}
         >
-          Innstillinger
+          Admin
         </NavLink>
         <NavLink className="logoutBtn" to="/">
           Log out
